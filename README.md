68eb355f4564f8fb416276565808074b99e90e69
lingyixia <chinachenfeiyu@outlook.com>
2019-04-03 11:37:12 +0800

    first crf

---
+++ README.md
@@ -0,0 +1,9 @@
+# CRF-NER
+[参考代码](https://github.com/applenob/simple_crf)
+
+[介绍博客](https://applenob.github.io/crf.html)
+
+## 执行
+`python3 example.py`
+该代码只用于自己理解
+
---
+++ Test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2019/4/3 10:04
+# @Author : 陈飞宇
+# @File : Test.py
+# @Software: PyCharm
+
+a = ['NNS', 'RB', 'VBN', 'NNP', 'PRP$', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'IN', 'VBG', 'VBP', 'RB', 'VBN', 'VBG', 'CD', 'VBP', 'RB', 'VBN']
+b=  ['NN', 'IN', 'DT', 'NN', 'VBZ', 'RB', 'VBN', 'TO', 'VB', 'DT', 'JJ', 'NN', 'IN', 'NN', 'NNS', 'IN', 'NNP', ',', 'JJ', 'IN', 'NN', 'NN', ',', 'VB', 'TO', 'VB', 'DT', 'JJ', 'NN', 'IN', 'NNP', 'CC', 'NNP', 'POS', 'JJ', 'NNS', '.']
+print(len(a))
+print(len(b))
